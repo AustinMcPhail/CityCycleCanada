681140aa18ec2747098d7eb0a29e55e1f4cf2b34
Nicolas Roussel-Lewis <nicolasrlewis@gmail.com>
2017-10-29 11:33:09 -0600

    The new initial commit. Still using the kitkit api version. We have to use the javascript version of google maps, because the native android version does not support layers (including the bicycling layer). Unfortunately that means we have to bassically load an html webpage inside a view and we can't use features in the android api.

---
+++ CityCycleCanada/app/src/main/java/city/cycle/canada/homeScreen.kt
@@ -0,0 +1,12 @@
+package city.cycle.canada
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class homeScreen : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_screen)
+    }
+}
