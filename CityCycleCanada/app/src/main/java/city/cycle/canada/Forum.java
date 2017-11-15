e39b8fb4a184942a00cd7e53334179fef7035946
Nicolas <nicolasrlewis@gmail.com>
2017-11-15 12:58:43 -0600

    Made the navigation bar work in all new acitivities (minus the google sign in, to come in my next commit). Pulled out the toolbar and drawer layouts into seperate files to eliminate duplication. Drawer related code in the Java files should also be pulled out into a central class to also eliminate duplication.

--- CityCycleCanada/app/src/main/java/city/cycle/canada/Forum.java
+++ CityCycleCanada/app/src/main/java/city/cycle/canada/Forum.java
@@ -1,13 +1,21 @@
 package city.cycle.canada;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 
-public class Forum extends AppCompatActivity {
+public class Forum extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +23,37 @@ public class Forum extends AppCompatActivity {
         setContentView(R.layout.activity_forum);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.forum_activity);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
     }
 
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        //Handle menu option actions
+        if (id == R.id.list_stolen_bike) {
+            Intent intent = new Intent(Forum.this, StolenBike.class);
+            startActivity(intent);
+        } else if (id == R.id.forum) {
+            Intent intent = new Intent(Forum.this, Forum.class);
+            startActivity(intent);
+
+        } else {
+            //Shouldn't happen
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.forum_activity);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
 }
