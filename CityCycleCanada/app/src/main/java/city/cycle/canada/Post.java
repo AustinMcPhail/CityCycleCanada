3e86197d39722bbded327875be4912d4ea843dab
Nicolas <nicolasrlewis@gmail.com>
2017-11-19 11:53:54 -0600

    Added OnClick EventListener to the individual post buttons. Also added the activity for the indvidual posts

---
+++ CityCycleCanada/app/src/main/java/city/cycle/canada/Post.java
@@ -0,0 +1,111 @@
+package city.cycle.canada;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.tasks.Task;
+
+import src.city.cycle.canada.GoogleSignInService;
+
+import static src.city.cycle.canada.Constants.RC_SIGN_IN;
+
+public class Post extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener, View.OnClickListener {
+
+    private GoogleSignInService googleSignIn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.post_activity);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        View header = navigationView.getHeaderView(0);
+        header.findViewById(R.id.sign_in_button).setOnClickListener(this);
+        header.findViewById(R.id.logout_button).setOnClickListener(this);
+
+        googleSignIn = new GoogleSignInService(this,this );
+
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        googleSignIn.refreshGoogleSignInUI(account);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        //Handle menu option actions
+        if (id == R.id.list_stolen_bike) {
+            Intent intent = new Intent(Post.this, StolenBike.class);
+            startActivity(intent);
+        } else if (id == R.id.forum) {
+            Intent intent = new Intent(Post.this, Forum.class);
+            startActivity(intent);
+
+        } else {
+            //Shouldn't happen
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.post_activity);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    //Google sign in code
+    @Override
+    public void onClick(View v){
+        switch (v.getId()) {
+            case R.id.sign_in_button:
+                googleSignIn.signIn();
+                break;
+            case R.id.logout_button:
+                googleSignIn.signOut();
+                break;
+            default:
+                //This should never happen
+                break;
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            googleSignIn.handleSignInResult(task);
+        }
+    }
+
+
+}
--- CityCycleCanada/app/src/main/java/src/city/cycle/canada/ForumPostAdapter.java
+++ CityCycleCanada/app/src/main/java/src/city/cycle/canada/ForumPostAdapter.java
@@ -1,6 +1,7 @@
 package src.city.cycle.canada;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,15 +10,22 @@ import android.widget.Button;
 
 import java.util.ArrayList;
 
+import city.cycle.canada.Forum;
+import city.cycle.canada.Post;
 import city.cycle.canada.R;
+import city.cycle.canada.StolenBike;
 
 /**
  * Created by nicolas on 18/11/17.
  */
 
 public class ForumPostAdapter extends ArrayAdapter<ForumPost> {
+
+    private Context context;
+
     public ForumPostAdapter(Context context, ArrayList<ForumPost> posts) {
         super(context, 0, posts);
+        this.context = context;
     }
 
     @Override
@@ -32,6 +40,21 @@ public class ForumPostAdapter extends ArrayAdapter<ForumPost> {
         Button postView = (Button) convertView.findViewById(R.id.forum_post);
         // Populate the data into the template view using the data object
         postView.setText(post.title);
+
+        postView.setTag(position);
+
+        postView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int position = (Integer) view.getTag();
+                // Access the row position here to get the correct data item
+                ForumPost post = getItem(position);
+                // Do what you want here...
+                System.out.println("OnClick has Fired");
+                Intent intent = new Intent(context, Post.class);
+                context.startActivity(intent);
+            }
+        });
         // Return the completed view to render on screen
         return convertView;
     }
