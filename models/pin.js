19897dbc78868576ad54948159fc765da7409c7c
Austin McPhail <austin.mcphail@hotmail.com>
2017-11-24 08:30:44 -0600

    Removed Pin model file.

--- models/pin.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-var pinSchema = new mongoose.Schema({
-    id : {type: Number, unique:true, index:true, required:true},
-    latitude : {type:Number, unique:false, required:true},
-    longitude : {type:Number, unique:false, required:true},
-    created: {type: Date, default:Date.now}
-});
-
-var Pin = module.exports = mongoose.model('Pin', pinSchema);
